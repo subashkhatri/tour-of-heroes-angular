05bc40dd828f1c18fc8b70a8811dfc2bbc31f57a
Subash Khatri <subasskhatrii@gmail.com>
2020-10-06 21:00:49 +0545

    initial commit   - added heroes with its component,css and typescript file   - created heroes-mock to add default datas   - added hero-details to list out the all heroes along with its details

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'subash'},
+  {id: 12, name: 'khatri'},
+  {id: 13, name: 'narayan'},
+  {id: 14, name: 'om'},
+  {id: 15, name: 'shivaye'},
+  {id: 16, name: 'rudra'},
+  {id: 17, name: 'kedarnath'},
+  {id: 18, name: 'nilkanda'},
+  {id: 19, name: 'devokedev'},
+  {id: 20, name: 'mahadev'},
+];
